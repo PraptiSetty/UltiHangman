9a111dce87ce0ca5ef04be167b48735c4d694576
Prapt Setty <settyp@tcd.ie>
2016-11-30 18:13:09 +0000

    Update UltiHangman.java

--- src/UltiHangman.java
+++ src/UltiHangman.java
@@ -1,46 +1,355 @@
-import java.util.Scanner ;
+import java.util.Scanner;
+import java.util.Arrays;
+public class UltiHangman {
 
-public class UltiHangman 
-{
-	public static void main (String [] args ) 
-	{	
+    static String usedLetters = "";
+    static Scanner inputScanner = new Scanner(System.in);
 
-	}
-	public static String getWord()
-	{
-		System.out.println("USER 1");
-		System.out.println("WHAT WORD DO YOU WANT USER 2 TO GUESS? : ");
-		Scanner inputScanner = new Scanner( System.in );
-		String word = inputScanner.next();
-		
-		inputScanner.close();
-		return word;
-	}
-	public static String getClue()
-	{
-		System.out.println("USER 1");
-		System.out.println("GIVE US A CLUE TO PROVIDE USER 2 WITH : ");
-		Scanner inputScanner = new Scanner( System.in );
-		String clue = inputScanner.next();
-		
-		inputScanner.close();
-		return clue;
-	}
-	public static boolean printUnderscores(String word) 
-	{
-		boolean underscorePrinted = false;
-		for (int i =0; i < word.length(); i++)
-		{
-			System.out.print(" _ ");
-		}
-		return underscorePrinted;	    
-	}
-	public static boolean clearScreen()
-	{
-		for (int o = 0; o < 24; o++)
-		{
-			System.out.println("  ");
-		}
-		return true;
-	}
+    public static void main(String[] args)
+    {
+        int mistakes = 0;
+        int guesses = 0;
+        String word = getWord();
+        String clue = getClue();
+        String obfusticatedWord = obWords(word);
+        clearScreen();
+
+        while (mistakes < 8)
+        {
+            printHangMan(mistakes);
+            System.out.println("" );
+            System.out.println(obfusticatedWord);
+            System.out.println("Wrong guesses: " + mistakes + " Guesses: " + guesses + " Used letters: " + usedLetters);
+            String letter = getLetter();
+            if (letterUsed(letter))
+            {
+                System.out.println("That Letter has been used");
+                mistakes++;
+            }
+            else if (word.contains(letter))
+            {
+                obfusticatedWord = revealLetter(letter, word, obfusticatedWord);
+                System.out.println("That Letter is correct!");
+
+            }
+            else
+            {
+                System.out.println("Not a letter.");
+                mistakes++;
+            }
+            guesses++;
+            if (word.equals(obfusticatedWord)) break;
+            if (guesses > 4) System.out.println("The clue: " + clue);
+        }
+
+        gameFinished(guesses, mistakes, word);
+
+    }
+
+    private static boolean letterUsed(String letter)
+    {
+
+        if (usedLetters.contains(letter))
+        {
+            return true;
+        }
+        else {
+            usedLetters = usedLetters + letter;
+            char [] c = usedLetters.toCharArray();
+            Arrays.sort(c);
+            usedLetters = new String(c);
+            return false;
+        }
+    }
+
+    public static String getWord()
+    {
+
+        System.out.println("CHALLENGER!");
+        System.out.println("WHAT WORD DO YOU WANT YOUR OPPONENT TO GUESS? : ");
+        String word = inputScanner.next();
+
+        return word;
+    }
+
+    public static String getClue()
+    {
+    	System.out.println("CHALLENGER!");
+    	System.out.println("GIVE US A CLUE TO PROVIDE YOUR STRUGGLING OPPONENT WITH : ");
+        inputScanner.nextLine();
+        String clue = inputScanner.nextLine();
+
+        return clue;
+    }
+
+    public static String obWords(String word)
+    {
+        String obfusticatedWord = "";
+        for (int i =0; i < word.length(); i++)
+        {
+            obfusticatedWord += "_";
+        }
+        return obfusticatedWord;
+    }
+
+    public static boolean clearScreen()
+    {
+        for (int o = 0; o < 24; o++)
+        {
+            System.out.println("  ");
+        }
+        return true;
+    }
+
+    public static void printHangMan(int mistake)
+    {
+        switch (mistake)
+        {
+            case 0:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("********************************      *");
+                System.out.println("********************************      *");
+                System.out.println("***************************************");
+                break;
+            case 1:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************    **************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("*****************  ***************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("********************************      *");
+                System.out.println("********************************      *");
+                System.out.println("***************************************");
+                break;
+            case 2:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************    **************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("*****************  ***************  ***");
+                System.out.println("**************        ************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("********************************      *");
+                System.out.println("********************************      *");
+                System.out.println("***************************************");
+                break;
+            case 3:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************    **************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("*****************  ***************  ***");
+                System.out.println("**************        ************  ***");
+                System.out.println("************  *      *************  ***");
+                System.out.println("**********  ***      *************  ***");
+                System.out.println("********   ****      *************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("********************************      *");
+                System.out.println("********************************      *");
+                System.out.println("***************************************");
+                break;
+            case 4:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************    **************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("*****************  ***************  ***");
+                System.out.println("**************        ************  ***");
+                System.out.println("************  *      *  **********  ***");
+                System.out.println("**********  ***      ***  ********  ***");
+                System.out.println("********   ****      ****   ******  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("********************************      *");
+                System.out.println("********************************      *");
+                System.out.println("***************************************");
+                break;
+            case 5:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************    **************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("*****************  ***************  ***");
+                System.out.println("**************        ************  ***");
+                System.out.println("************  *      *  **********  ***");
+                System.out.println("**********  ***      ***  ********  ***");
+                System.out.println("********   ****      ****   ******  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("**************   *****************  ***");
+                System.out.println("*************   ******************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("********************************      *");
+                System.out.println("********************************      *");
+                System.out.println("***************************************");
+                break;
+            case 6:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************    **************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("*****************  ***************  ***");
+                System.out.println("**************        ************  ***");
+                System.out.println("************  *      *  **********  ***");
+                System.out.println("**********  ***      ***  ********  ***");
+                System.out.println("********   ****      ****   ******  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("**************   *    ************  ***");
+                System.out.println("*************   ***   ************  ***");
+                System.out.println("**********************************  ***");
+                System.out.println("********************************      *");
+                System.out.println("********************************      *");
+                System.out.println("***************************************");
+                break;
+            case 7:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************    **************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("*****************  ***************  ***");
+                System.out.println("**************        ************  ***");
+                System.out.println("************  *      *  **********  ***");
+                System.out.println("**********  ***      ***  ********  ***");
+                System.out.println("********   ****      ****   ******  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("**************   *    ************  ***");
+                System.out.println("*************   ***   ************  ***");
+                System.out.println("************   *******************  ***");
+                System.out.println("**********   *******************      *");
+                System.out.println("*******     ********************      *");
+                System.out.println("***************************************");
+                break;
+            case 8:
+                System.out.println("***************************************");
+                System.out.println("******************                  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************** ***************  ***");
+                System.out.println("****************    **************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("*****************  ***************  ***");
+                System.out.println("**************        ************  ***");
+                System.out.println("************  *      *  **********  ***");
+                System.out.println("**********  ***      ***  ********  ***");
+                System.out.println("********   ****      ****   ******  ***");
+                System.out.println("***************      *************  ***");
+                System.out.println("**************   *    ************  ***");
+                System.out.println("*************   ***   ************  ***");
+                System.out.println("************   *****   ***********  ***");
+                System.out.println("**********   ********   ********      *");
+                System.out.println("*******     ***********    *****      *");
+                System.out.println("***************************************");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static String getLetter()
+    {
+        boolean validLetter = false;
+        Scanner inputScanner = new Scanner(System.in);
+
+        while (!validLetter)
+        {
+            String input = inputScanner.next();
+
+            if (input.length() == 1)
+            {
+                return input;
+            }
+            else
+            {
+                System.out.print("Sorry, you entered an invalid letter. Please try again.");
+            }
+        }
+        return "";
+    }
+
+    public static void gameFinished(int numberOfGuesses, int numberOfWrongGuesses, String word)
+    {
+
+
+        if (numberOfWrongGuesses == 8)
+        {
+            System.out.println("Unfortunate. You lost the game! Better luck next time! The word inputted by your nemesis was" + word +".");
+        }
+        else
+        {
+            System.out.println("You have won the game! WELL DONE!! It took you "
+                    + numberOfGuesses +" guesses to get the right word! The word inputted by your Challenger was \"" + word +"\"." );
+        }
+
+    }
+
+    public static String revealLetter(String letter, String word, String hword)
+    {
+        String nhword = hword;
+        String nahword = "";
+        for (int i = 0; i < word.length(); i ++)
+        {
+            if (word.charAt(i) == letter.charAt(0))
+            {
+                
+                nahword = nhword.substring(0, i) + letter + nhword.substring(i+1);
+                nhword = nahword;
+            }
+        }
+        return nhword;
+    }
+
 }
